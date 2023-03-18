f95f156d13a8d0bf5ab41f9582deb2ca921d726a
sam <samuelmebersole@gmail.com>
2023-03-18 14:17:42 +0100

    Mock responses to messages

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -8,18 +8,30 @@ import './App.css';
 
 const SERVER = {
   frontend: {
-    users: [],
+    users: ['localUser', 'userA', 'userB'],
     chat: [
       { user: 'userA', text: 'Test frontend message A' },
       { user: 'userB', text: 'Test frontend message B' },
     ],
   },
   backend: {
-    user: [],
+    user: ['localUser', 'userC'],
     chat: [
       { user: 'userC', text: 'Test backend message A' },
     ],
   },
+};
+
+function mockResponse(channel, sendMessage) {
+  const getRandomInt = (max) => {
+    return Math.floor(Math.random() * max);
+  };
+  const willRespond = getRandomInt(2);
+  if (!willRespond) { return; }
+  const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque venenatis cursus nulla rhoncus dignissim. In facilisis, erat sit amet elementum tempus, velit tortor pellentesque odio, et accumsan mi nulla id massa. Sed iaculis neque odio. Duis luctus lectus vel ornare tempor. Nam in molestie lorem, sed placerat ipsum. Fusce condimentum ultrices feugiat. Fusce maximus dui ex, non tincidunt nunc convallis in.'.split(' ');
+  const words = lorem.length;
+  const sender = 'userC';
+  sendMessage(lorem.slice(getRandomInt(words)).join(' '), sender);
 };
 
 function App() {
@@ -27,14 +39,20 @@ function App() {
   const [chat, setChat] = useState(SERVER[channel].chat);
   const [user] = useState('localUser');
 
-  const sendMessage = (text) => {
+  const sendMessage = (text, sender = user) => {
     const nChat = [...chat];
-    nChat.push({ user, text });
+    nChat.push({ user: sender, text });
     setChat(nChat);
     SERVER[channel].chat = nChat;
   };
 
-  useEffect(() => { setChat(SERVER[channel].chat); }, [channel]);
+  useEffect(() => {
+    setChat(SERVER[channel].chat);
+  }, [channel]);
+
+  useEffect(() => {
+    mockResponse(channel, sendMessage);
+  }, [chat.length]);
 
   return (
     <div className="app">
